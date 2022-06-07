5512d0f763a61bdb0f4d19a0dc2e1364ede426b8
Bjorn Madsen <root-11@users.noreply.github.com>
2022-06-07 11:54:58 +0100

    Update test_basics.py
    Added broken task to the test suite.

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -5,16 +5,28 @@ import time
 def test_alpha():
     args = list(range(10)) * 5
     start = time.time()
-    tasks = [Task(f, *(arg/10,), **{'hello': arg}) for arg in args]
+    
     with TaskManager() as tm:
+        # add the first tasks
+        tasks = [Task(f, *(arg/10,), **{'hello': arg}) for arg in args]    
+        results = tm.execute(tasks)  
+        
+        end = time.time()
+        print(f"did nothing for {end-start} seconds, producing {results} result")
+        
+        # add more tasks to the same pool of workers:
+        tasks = [Task(broken, *(i,) for i in range(3)]
         results = tm.execute(tasks)
-    end = time.time()
-    print(f"did nothing for {end-start} seconds, producing {results} result")
+        for result in results:
+            print(result)  # this will print tracebacks!
 
 def f(*args, **kwargs):
     print(args, kwargs)
     time.sleep(args[0])
     return args[0]
+
+def broken(*args, **kwargs):
+    raise NotImplementedError("this task must fail!")
 
 
 if __name__ == "__main__":
