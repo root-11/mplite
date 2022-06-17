56d6141b9c1b7d2221631fb2b88479601d04decd
root-11 <dr.bjorn.madsen@gmail.com>
2022-06-17 10:57:09 +0100

    updated test to show error handler

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -8,22 +8,29 @@ def test_alpha():
     
     with TaskManager() as tm:
         # add the first tasks
-        tasks = [Task(f, *(arg/10,), **{'hello': arg}) for arg in args]    
-        results = tm.execute(tasks)  
+        tasks = [Task(f, *(arg/10,), **{'hello': arg}) for arg in args]
+
+        print("an example of a tasks is available as string:\n\t", str(tasks[0]), '\n\t', repr(tasks[0]))
+
+        results = tm.execute(tasks)   # this will contain results and tracebacks!
         
         end = time.time()
-        print(f"did nothing for {end-start} seconds, producing {results} result")
+        print(f"did nothing for {end-start} seconds, producing {len(results)} results")
+        print(f"hereof {len([result for result in results if isinstance(result, str) ])} had errors.")
+        print(f"the rest where results: {[i for i in results if not isinstance(i,str)]}")
         
-        # add more tasks to the same pool of workers:
+        # add more tasks to the SAME pool of workers:
         tasks = [Task(broken, *(i,)) for i in range(3)]
         results = tm.execute(tasks)
+        print("More expected errors:")
         for result in results:
-            print(result)  # this will print tracebacks!
+            print("expected -->", result)  
+
 
 def f(*args, **kwargs):
-    print(args, kwargs)
     time.sleep(args[0])
-    return args[0]
+    return args[0]/kwargs['hello']
+
 
 def broken(*args, **kwargs):
     raise NotImplementedError("this task must fail!")
