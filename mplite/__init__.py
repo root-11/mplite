f7f224a450dedd4e4ba26a45ec51fa2b6287e6ff
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-11-23 11:52:04 +0000

    bump version number for maintenance release.

--- mplite/__init__.py
+++ mplite/__init__.py
@@ -6,7 +6,7 @@ from tqdm import tqdm as _tqdm
 import queue
 
 
-major, minor, patch = 1, 2, 0
+major, minor, patch = 1, 2, 1
 __version_info__ = (major, minor, patch)
 __version__ = '.'.join(str(i) for i in __version_info__)
 
