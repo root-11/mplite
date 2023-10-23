0f83639157717d0539a26c99a38eaaea453906d7
Ratchet <audryzas@homtail.com>
2023-10-23 13:29:06 +0300

    * bump version

--- mplite/__init__.py
+++ mplite/__init__.py
@@ -8,7 +8,7 @@ import queue
 from itertools import count
 
 
-major, minor, patch = 1, 2, 4
+major, minor, patch = 1, 2, 5
 __version_info__ = (major, minor, patch)
 __version__ = '.'.join(str(i) for i in __version_info__)
 default_context = "spawn"
