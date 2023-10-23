302b1313c7ce5b0be844080243e0a979df0675fd
Ratchet <audryzas@homtail.com>
2023-10-23 13:10:37 +0300

    need to locate that semaphore on 311

--- mplite/__init__.py
+++ mplite/__init__.py
@@ -11,7 +11,7 @@ from itertools import count
 major, minor, patch = 1, 2, 4
 __version_info__ = (major, minor, patch)
 __version__ = '.'.join(str(i) for i in __version_info__)
-default_context = None if "pytest" in sys.modules else "spawn"
+default_context = "spawn"
 
 class Task(object):
     task_id_counter = count(start=1)
